f9a2a31dcb8f5e43f12aa113bd3227ca52fb0207
Charles Fernandez <charliefernandez777@gmail.com>
2019-12-03 17:17:18 -0500

    Initial commit

---
+++ Models/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondCharliesTechShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
---
+++ Models/ITechRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondCharliesTechShop.Models
+{
+    interface ITechRepository
+    {
+        IEnumerable<Tech> AllTech { get; }
+        IEnumerable<Tech> MostPopularTech { get; }
+        Tech GetTechById(int techId);
+    }
+}
