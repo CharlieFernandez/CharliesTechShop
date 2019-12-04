1aeece96e05fbe9ef737a8fd97ff47b2d5808b15
Charles Fernandez <charliefernandez777@gmail.com>
2019-12-04 16:46:14 -0500

    Stylized the View

--- Models/Tech.cs
+++ Models/Tech.cs
@@ -9,11 +9,8 @@ namespace SecondCharliesTechShop.Models
     {
         public int TechId { get; set; }
         public string Name { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-        public string ImageThumbnailUrl { get; set; }
         public bool PopularTech { get; set; }
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
