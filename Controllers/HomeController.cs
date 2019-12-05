1aa3fe0b9969cd4b37e7a835e9b955e069b336e6
Charles Fernandez <charliefernandez777@gmail.com>
2019-12-04 22:26:12 -0500

    Created Home page and added Partial Views

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SecondCharliesTechShop.Models;
+using SecondCharliesTechShop.ViewModels;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SecondCharliesTechShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITechRepository _techRepository;
+
+        public HomeController(ITechRepository techRepository)
+        {
+            _techRepository = techRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                MostPopularTech = _techRepository.MostPopularTech
+            };            
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using SecondCharliesTechShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondCharliesTechShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Tech> MostPopularTech { get; set; }
+    }
+}
