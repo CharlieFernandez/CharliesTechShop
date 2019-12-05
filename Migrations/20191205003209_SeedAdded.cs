92ed18eb55a2a88aed61ff3dddc2aaca9aa8cdb1
Charles Fernandez <charliefernandez777@gmail.com>
2019-12-04 19:34:43 -0500

    Created and initialized database

---
+++ Migrations/20191205003209_SeedAdded.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecondCharliesTechShop.Migrations
+{
+    public partial class SeedAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[] { 1, "Laptops", null });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[] { 2, "Computer Mice", null });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[] { 3, "Mouse Pads", null });
+
+            migrationBuilder.InsertData(
+                table: "Tech",
+                columns: new[] { "TechId", "CategoryId", "ImageUrl", "InStock", "Name", "PopularTech", "Price" },
+                values: new object[,]
+                {
+                    { 1, 1, "https://images-na.ssl-images-amazon.com/images/I/81H9y1pPW6L._AC_SL1500_.jpg", true, "Acer Laptop", true, 799.99m },
+                    { 2, 2, "https://images-na.ssl-images-amazon.com/images/I/61d9C4yCB2L._AC_SL1000_.jpg", true, "PICTEK Gaming Mouse", false, 15.29m },
+                    { 4, 2, "https://images-na.ssl-images-amazon.com/images/I/81ZQgWwuVzL._AC_SL1500_.jpg", true, "Logitech G602 Mouse", false, 24.99m },
+                    { 3, 3, "https://images-na.ssl-images-amazon.com/images/I/51Wf5oBCB1L._AC_SL1000_.jpg", true, "Teemo Mouse Pad", false, 10.99m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tech",
+                keyColumn: "TechId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Tech",
+                keyColumn: "TechId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tech",
+                keyColumn: "TechId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tech",
+                keyColumn: "TechId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+        }
+    }
+}
--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -15,5 +15,58 @@ namespace SecondCharliesTechShop.Models
 
         public DbSet<Tech> Tech { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 1, CategoryName = "Laptops" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 2, CategoryName = "Computer Mice" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 3, CategoryName = "Mouse Pads" });
+
+            modelBuilder.Entity<Tech>().HasData(new Tech
+            {
+                TechId = 1,
+                Name = "Acer Laptop",
+                Price = 799.99M,
+                CategoryId = 1,
+                ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/81H9y1pPW6L._AC_SL1500_.jpg",
+                InStock = true,
+                PopularTech = true
+            });
+
+            modelBuilder.Entity<Tech>().HasData(new Tech
+            {
+                TechId = 2,
+                Name = "PICTEK Gaming Mouse",
+                Price = 15.29M,
+                CategoryId = 2,
+                ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/61d9C4yCB2L._AC_SL1000_.jpg",
+                InStock = true,
+                PopularTech = false
+            });
+
+            modelBuilder.Entity<Tech>().HasData(new Tech
+            {
+                TechId = 3,
+                Name = "Teemo Mouse Pad",
+                Price = 10.99M,
+                CategoryId = 3,
+                ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/51Wf5oBCB1L._AC_SL1000_.jpg",
+                InStock = true,
+                PopularTech = false
+            });
+
+            modelBuilder.Entity<Tech>().HasData(new Tech
+            {
+                TechId = 4,
+                Name = "Logitech G602 Mouse",
+                Price = 24.99M,
+                CategoryId = 2,
+                ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/81ZQgWwuVzL._AC_SL1500_.jpg",
+                InStock = true,
+                PopularTech = false
+            });
+        }
     }
 }
